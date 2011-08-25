e253091be762b0ba4c89e2a0aab754f860bd8783
zhdanov <yuriy.zhdanov@gmail.com>
2011-08-25 21:09:13 +0400

    Виртуальные хосты создаются, apache перезагружается

--- classes/Apache.class.php
+++ classes/Apache.class.php
@@ -1,6 +1,47 @@
 <?php
 require_once(dirname(__FILE__).'/../lib/Smarty/Smarty.class.php');
+require_once(dirname(__FILE__).'/../classes/Settings.class.php');
 
 class Apache {
+  private $settings = array();
+  private $smarty   = false;
 
+  private $fileVirtualhost   = 'virtualhost.tpl';
+  private $helpForCreateHost = 'apachehost     Создание виртуального хоста в apache';
+
+  public function __construct() {
+    $settings       = new Settings();
+    $this->smarty   = new Smarty();
+    $this->settings = $settings->getArray();
+  }
+
+  private function getVirtualHost($hostname) {
+    $this->smarty->assign('hostname', $hostname);
+    $this->smarty->assign('email', $this->settings['email']);
+    $this->smarty->assign('wwwdir', $this->settings['wwwdir']);
+    return $this->smarty->fetch($this->fileVirtualhost);
+  }
+
+  private function createSitesAvailable($hostname) {
+    $puthfile    = $this->settings['apache']['sites_available'].$hostname;
+    $virtualhost = $this->getVirtualHost($hostname);
+    `echo '$virtualhost' >> $puthfile`;
+  }
+
+  private function createSitesEnabled($hostname) {
+    $puthfileAvailable    = $this->settings['apache']['sites_available'].$hostname;
+    $puthfileEnabled    = $this->settings['apache']['sites_enabled'].$hostname;
+    `ln -s $puthfileAvailable $puthfileEnabled`;
+  }
+
+  private function restart() {
+    `apache2ctl stop`;
+    `apache2ctl start`;
+  }
+
+  public function createHost($hostname) {
+    $this->createSitesAvailable($hostname);
+    $this->createSitesEnabled($hostname);
+    $this->restart();
+  }
 }
--- smarty/templates/virtualhost.tpl
+++ smarty/templates/virtualhost.tpl
@@ -1,1 +1,25 @@
-{$hostname}
+<VirtualHost *:80>
+	ServerAdmin {$email}
+	ServerName {$hostname}
+
+	DocumentRoot {$wwwdir}{$hostname}/web
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride None
+	</Directory>
+	<Directory {$wwwdir}{$hostname}/web/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride All
+		Order allow,deny
+		allow from all
+	</Directory>
+
+	ErrorLog /var/log/apache2/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog /var/log/apache2/access.log combined
+
+</VirtualHost>
