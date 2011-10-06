b857ffd54f75a274af96cfe59ac79d5116b132fb
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-06 22:44:38 +0400

    Создаётся www директория

--- classes/Wwwdir.class.php
+++ classes/Wwwdir.class.php
@@ -1,4 +1,17 @@
 <?php
+require_once(dirname(__FILE__).'/../classes/Settings.class.php');
+
 class Wwwdir {
+  private $settings = array();
+
+  public function __construct() {
+    $settings       = new Settings();
+    $this->settings = $settings->getArray();
+  }
+
+  public function createDir($dirname) {
+    $dirpath = $this->settings['wwwdir'] . $dirname . '/web/';
+    `mkdir -p $dirpath`;
+  }
 
 }
