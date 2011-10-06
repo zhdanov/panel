23f87bbfd369766556e14563b250823ab8bb173b
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-07 00:32:07 +0400

    Создание базы и пользователи с правами на неё в mysql

--- classes/Mysql.class.php
+++ classes/Mysql.class.php
@@ -1,4 +1,27 @@
 <?php
+require_once(dirname(__FILE__).'/../classes/Settings.class.php');
+
 class Mysql {
+  private $settings = array();
+
+  public function __construct() {
+    $settings       = new Settings();
+    $this->settings = $settings->getArray();
+  }
+
+  public function createDb($name) {
+    $command = 'echo "create database ' . $name . '" | mysql --user=' . $this->settings['mysql']['user'] . ' --password="' . $this->settings['mysql']['password'] . '"';
+    `$command`;
+  }
+
+  public function createUser($name) {
+    $command = 'echo "GRANT ALL PRIVILEGES ON ' . $name . '.* TO ' . $name . '@localhost IDENTIFIED BY ' ."'".$name."'".';" | mysql --user=' . $this->settings['mysql']['user'] . ' --password="' . $this->settings['mysql']['password'] . '"';
+    `$command`;
+  }
+
+  public function createDbuser($name) {
+    $this->createDb($name);
+    $this->createUser($name);
+  }
 
 }
