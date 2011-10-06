ac1b34444197299c736e42a6eef3a52174d35302
zhdanov <yuriy.zhdanov@gmail.com>
2011-10-06 20:13:09 +0400

    Добавлен help по использованию console.php

--- classes/DefaultHelp.class.php
+++ classes/DefaultHelp.class.php
@@ -1,6 +1,15 @@
 <?php
 class DefaultHelp {
   public function getForAll() {
+    $result = "Использование: sudo php console.php КОМАНДА [АРГУМЕНТЫ]\n\n"
+            . "Популярные команды:\n"
+            . "   create\n"
+            . "     apachehost    Создать виртуальный хост в apache\n"
+            . "     wwwdir        Создать www директорию\n"
+            . "     etchost       Создать запись в /etc/host\n"
+            . "     mysqldbuser   Создать базу и пользователя в mysql\n"
+            . "     set           Создать всё перечисленное выше";
 
+    return $result; 
   }
 }
